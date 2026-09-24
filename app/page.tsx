461e3a54e13a61c4fb4c8f5cfd9821368f8e7ff9
Lalit Singh <nlalitsingh2014@gmail.com>
2026-09-24 09:48:27 +0545

    bug: change the path of image logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from '../storage/team/logo.png'
+import logo from '@/public/images/logo.png'
 import {
   ArrowRight,
   BadgeCheck,
